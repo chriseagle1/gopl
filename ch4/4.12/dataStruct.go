09cc710f3c92c244016e38faf17ba233710f65e0
chriseagle1 <christemp@163.com>
2020-08-19 14:25:06 +0800

    4.12

---
+++ ch4/4.12/dataStruct.go
@@ -0,0 +1,15 @@
+package main
+
+type Comics struct {
+	Month string
+	Num int
+	Link string
+	Year string
+	News string
+	SafeTitle string `json:"safe_title"`
+	Transcript string
+	Alt string
+	img string
+	Title string
+	Day string
+}
